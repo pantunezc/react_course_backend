dcadc80121885a107e0bd687ab5c54c5c8d800c2
Pau Antúnez <pau.antunez.clavero@gmail.com>
2024-04-05 21:12:32 +0200

    exercice 3.9

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ let persons = [
 ];
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body "));
-
+app.use(express.static("dist"));
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
 });
