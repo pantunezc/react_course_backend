bfa17b98e917c681fd91f5de5c6ea44f95e8c3fd
Pau Antúnez <pau.antunez.clavero@gmail.com>
2024-04-10 20:09:22 +0200

    exercice 3.14

--- index.js
+++ index.js
@@ -5,28 +5,7 @@ const morgan = require("morgan");
 
 const Person = require("./models/person");
 
-let persons = [
-  /* {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  }, */
-];
+let persons = [];
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body "));
 app.use(express.static("dist"));
@@ -38,9 +17,9 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-const generateId = () => {
+/* const generateId = () => {
   return Math.floor(Math.random() * 1000);
-};
+}; */
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
@@ -51,23 +30,22 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  const existingPerson = Person.find((person) => person.name === body.name);
+  /* const existingPerson = Person.find((person) => person.name === body.name);
 
   if (existingPerson) {
     return response.status(400).json({
       error: "name must be unique",
     });
-  }
+  } */
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 app.get("/api/persons", (request, response) => {
