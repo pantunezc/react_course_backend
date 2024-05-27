28ce0e22699ae8d2aa2bd2a1ada0cf24172ce435
Pau A. (Novaigrup) <pau.antunez@novaigrup.com>
2024-05-27 12:22:43 +0200

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const morgan = require("morgan");
 
+const app = express();
 const Person = require("./models/person");
 
 let persons = [];
+app.use(cors());
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body "));
 app.use(express.static("dist"));
