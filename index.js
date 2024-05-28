6f1eecfff1e292fd037288e44afba8107583d89c
Pau A. (Novaigrup) <pau.antunez@novaigrup.com>
2024-05-28 08:37:21 +0200

    exercice 3.19

--- index.js
+++ index.js
@@ -19,12 +19,14 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
 };
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -38,9 +40,12 @@ app.post("/api/persons", (request, response) => {
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
@@ -51,7 +56,7 @@ app.put("/api/persons/:id", (request, response, next) => {
     number,
   };
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: "query" })
     .then((updatedPerson) => {
       if (updatedPerson) {
         response.json(updatedPerson);
